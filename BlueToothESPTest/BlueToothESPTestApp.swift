15bc92be06319f66ea81c18311b944b60d089928
Paul Dommer <pdommer@coconut-capital.com>
2022-09-19 12:01:25 +0200

    Initial Commit

---
+++ BlueToothESPTest/BlueToothESPTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueToothESPTestApp.swift
+//  BlueToothESPTest
+//
+//  Created by Paul Dommer on 19.09.22.
+//
+
+import SwiftUI
+
+@main
+struct BlueToothESPTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
